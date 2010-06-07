9fcf66e146b3a803fab3b9e3694bbde1f188834c
yhamigaki <takeshi.mouri.net@green.ocn.ne.jp>
2010-06-08 07:08:51 +0900

    MAX_PATH -> PATH_MAX

--- compat/linux.cc
+++ compat/linux.cc
@@ -8,12 +8,12 @@
 namespace mgit {
 	namespace fsutil {
 		ustring get_exec_path() {
-			size_t len = MAX_PATH;
+			size_t len = PATH_MAX;
 			while (1) {
 				vararray<char> buf(len);
 				int ret = readlink("/proc/self/exe", buf.c_array(), buf.size());
 				if (ret<0) throw std::runtime_error("couldn'd get exec-path");
-				if (ret<buf.size()) return buf.c_array();
+				if (static_cast<size_t>(ret)<buf.size()) return buf.c_array();
 				len *= 2;
 			}
 		}
